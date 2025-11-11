a427b45678a4f3424a36959508f8d96f452ed098
navvkhn <69992995+navvkhn@users.noreply.github.com>
2025-11-12 04:42:52 +0530

    Update app.py
    added line for png export fix import plotly.io as pio
    pio.kaleido.scope.chromium_args = ("--no-sandbox", "--disable-dev-shm-usage")

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
+pio.kaleido.scope.chromium_args = ("--no-sandbox", "--disable-dev-shm-usage")
 
 # --- RESET FUNCTION ---
 def reset_app():
